ad35f8ab82073ca86c8b372d0cb4bfc6b638469e
YN <1109225291@qq.com>
2023-03-05 11:03:30 +0800

    Initial commit

---
+++ src/main/java/com/takeout/T_O_Application.java
@@ -0,0 +1,15 @@
+package com.takeout;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class T_O_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(T_O_Application.class, args);
+        log.info("Starting succeed ...");
+    }
+}
