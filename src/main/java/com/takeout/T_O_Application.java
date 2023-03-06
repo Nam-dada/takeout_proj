c004bbce55a70c864e4cdf0009c9109955316c37
Nam-dada <nw1109225291@outlook.com>
2023-03-06 10:13:17 +0800

    day2: optimize the login function

--- src/main/java/com/takeout/T_O_Application.java
+++ src/main/java/com/takeout/T_O_Application.java
@@ -4,9 +4,11 @@ package com.takeout;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class T_O_Application {
     public static void main(String[] args) {
         SpringApplication.run(T_O_Application.class, args);
---
+++ src/main/java/com/takeout/fillter/LoginCheckFilter.java
@@ -0,0 +1,76 @@
+package com.takeout.fillter;
+
+import com.alibaba.fastjson.JSON;
+import com.takeout.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+@Slf4j
+public class LoginCheckFilter implements Filter{
+    //路径匹配器，支持通配符
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //Get the URI of this request
+        String requestURI = request.getRequestURI();// /backend/index.html
+
+        log.info("拦截到请求：{}",requestURI);
+
+        //The urls that not need to deal with.
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+
+        //Judge
+        boolean check = check(urls, requestURI);
+
+        if(check){
+            log.info("本次请求{}不需要处理",requestURI);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        if(request.getSession().getAttribute("employee") != null){
+            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        log.info("用户未登录");
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+
+    }
+
+    /**
+     * 路径匹配，检查本次请求是否需要放行
+     * @param urls
+     * @param requestURI
+     * @return
+     */
+    public boolean check(String[] urls,String requestURI){
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if(match){
+                return true;
+            }
+        }
+        return false;
+    }
+}
